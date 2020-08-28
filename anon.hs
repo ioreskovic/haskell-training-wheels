a3321a18916ab2586e9051dd6fc2403db5a1c203
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2020-08-28 18:23:41 +0200

    Chapter 7

---
+++ anon.hs
@@ -0,0 +1,11 @@
+module Anon where
+
+addOneOfOdd = \n -> case odd n of
+  True -> f n
+  False -> n
+  where f = \y -> y + 1
+
+addFive = \x -> \y -> (if x > y then y else x) + 5
+
+mFlip f x y = f y x
+
---
+++ cases.hs
@@ -0,0 +1,18 @@
+module Cases where
+
+functionC x y =
+  case (x > y) of
+  True -> x
+  False -> y
+
+ifEvenAdd2 n =
+  case (even n) of
+  True -> n + 2
+  _ -> n
+
+nums x =
+  case compare x 0 of
+  LT -> -1
+  GT -> 1
+  EQ -> 0
+
